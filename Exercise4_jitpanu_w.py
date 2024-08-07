865438368596ad927f51d67015d2de6dde1b3158
Jitpanu123 <son_701@hotmail.com>
2024-08-07 22:14:27 +0700

    Create Exercise4_jitpanu_w.py

---
+++ Exercise4_jitpanu_w.py
@@ -0,0 +1,10 @@
+a = 80
+b = 82.5
+c = 93
+d = 88.5
+
+print('---Your Score---')
+print('Foundation English : ',a)
+print('General Business : ',b)
+print('Introduction to Computer Systems : ',c)
+print('Computer Programming : ',d)
